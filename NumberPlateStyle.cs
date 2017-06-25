995640b92ff9ce332a65edacb5e058b91fa975bb
Ruffo <groothoff@united-mail.org>
2017-06-25 06:46:13 +0200

    Create NumberPlateStyle.cs

---
+++ NumberPlateStyle.cs
@@ -0,0 +1,13 @@
+
+namespace Enums
+{
+  public enum NumberPlateStyle
+  {
+    BlueRedFontWhite = 0,
+    YellowFontBlack = 1,
+    YellowFontDarkBlue = 2,
+    BlueRedFontWhite2 = 3,
+    BlueRedFontWhiteNoIcons = 4,
+    BlueWhiteFontWhiteRed = 5
+  }
+}
