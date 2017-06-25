581635099347f5681f8ba6bae9d61be29c807eea
Ruffo <groothoff@united-mail.org>
2017-06-25 04:21:27 +0200

    Weather added

---
+++ Enums/Weather.cs
@@ -0,0 +1,20 @@
+﻿namespace Enums
+{
+  public enum Weather
+  {
+    ExtraSunny = 0,
+    Clear = 1,
+    Clouds = 2,
+    Smog = 3,
+    Foggy = 4,
+    Overcast = 5,
+    Rain = 6,
+    Thunder = 7,
+    Lightrain = 8,
+    Smoggylightrain = 9,
+    Verylightsnow = 10,
+    Windylightsnow = 11,
+    Lightsnow = 12,
+    Unknown = 13
+  }
+}
